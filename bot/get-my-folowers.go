3a893d98909833ff19a80628649f440e5cfd65a6
Alexandr <kydin@ya.ru>
2020-02-17 12:17:04 +0700

    Fix сleanup followers map

--- bot/get-my-folowers.go
+++ bot/get-my-folowers.go
@@ -11,9 +11,6 @@ import (
 func (s *Strava) GetMyFollowers(c *parser.Client){
 	var headers = map[string]string{}
 
-	s.Followers = make([]string, 0)
-	s.FollowersInfo = make(map[string]string)
-
 	headers["authorization"] = "access_token " + s.authToken
 
 	var myFolowersUrl = strings.ReplaceAll(s.MapUrls["followers_url"], "{ATHLETE-ID}", s.athleteId)
@@ -28,6 +25,8 @@ func (s *Strava) GetMyFollowers(c *parser.Client){
 	err := json.Unmarshal([]byte(jsonData), &results)
 	c.CheckError(err)
 
+
+	s.Followers = []string{}
 	s.FollowersInfo = make(map[string]string)
 
 	for _, result := range results {
